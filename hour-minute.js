18876a7be801ff57bea89bf67199187caa99040f
islamajabul <ajabulmd1@gmail.com>
2023-02-04 01:28:00 +0800

    added

---
+++ hour-minute.js
@@ -0,0 +1,17 @@
+ function hourToMinute(hour){
+    minute = hour*60;
+    return minute;
+}
+
+const inputHour = 8;
+const outputMinute = hourToMinute(inputHour);
+console.log('Output minute-',  outputMinute);
+
+/* function hourToMinute(hour){
+    const minute = hour*60;
+    return minute;
+}
+
+const inputHour = 30;
+const outputMinute = hourToMinute(inputHour);
+console.log('Output minute-',outputMinute); */
---
+++ miles-km.js
@@ -0,0 +1,8 @@
+function milesToKilometers(miles){
+    const kilometer = miles*1.61;
+    return kilometer;
+}
+
+const kushtiaToDaulatpurMiles = 30;
+const kushtiaToDaulatpurKilometers = milesToKilometers(kushtiaToDaulatpurMiles);
+console.log('Kushtia to Daulatpur distance-',kushtiaToDaulatpurKilometers);
