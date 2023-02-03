9ada149594792674939de56beade0af6883d9bda
islamajabul <ajabulmd1@gmail.com>
2023-02-04 00:43:04 +0800

    inch to feet

---
+++ recap.js
@@ -0,0 +1,46 @@
+// 1. Variable
+var myName = 'Ajabul';
+
+// math operation: +,-,*,/
+// shorthand: +=, -=, *=, /=
+// ++, --
+
+// 2. Array
+var friends = ['Rinku','Raju','Abdullaha','Rabby'];
+
+var thirdFriend= friends[2];
+friends[3] = 'Dabbu';
+
+// 3. Conditional
+// >, <, >=, <=, ==, ===, !=, !==
+if(friends.length <2){
+    console.log('Friend nai')
+}
+else{
+
+}
+
+// 4. Loop
+var number = 0;
+while(number <=6){
+    console.log(number)
+    number++;
+}
+
+for(var i = 0; i<=10; i++){
+
+}
+
+// 5. Function
+function isMoonUp(time){
+    if(time>7){
+        return true;
+    }
+}
+
+// 6. Object
+var jantus = {
+    height: 60,
+    age: 21,
+    romanticism: 'heavvy'
+}
